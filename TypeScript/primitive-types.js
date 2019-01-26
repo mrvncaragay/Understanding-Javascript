997c8974b02d92117173e2b6168a1d655c239564
Marvin Caragay <mrvncaragay@gmail.com>
2019-01-25 17:01:02 -0800

    TypeScript

---
+++ TypeScript/primitive-types.js
@@ -0,0 +1,36 @@
+// Boolean
+var isDone = false;
+// Number
+var age = 30;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+console.log(binary);
+console.log(octal);
+// String
+var myName = "Borlog!";
+console.log(myName);
+console.log("" + myName);
+// Array
+var ageList = [1, 3, 2];
+console.log(ageList);
+var strList = ["john", "borlog"];
+console.log(strList);
+// Tupple
+var ageName;
+ageName = [21, 'solve'];
+console.log(ageName);
+console.log(ageName[1].substr(1)); //->ovlve
+//error
+//let nameAge: [string, number] = [21 ,'solve'];
+// Enu,
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 12] = "Red";
+    Color[Color["Green"] = 13] = "Green";
+    Color[Color["Blue"] = 14] = "Blue";
+})(Color || (Color = {}));
+var c = Color.Red;
+var colorName = Color[13];
+console.log(c);
+console.log(colorName);
---
+++ TypeScript/primitive-types.ts
@@ -0,0 +1,55 @@
+// Boolean
+
+let isDone: boolean = false;
+
+// Number
+
+let age: number = 30;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+console.log(binary);
+console.log(octal);
+
+
+// String
+
+let myName: string = "Borlog!";
+
+console.log(myName);
+console.log(`${myName}`);
+
+// Array
+let ageList: number[] = [1, 3, 2];
+
+console.log(ageList);
+
+let strList: string[] = ["john", "borlog"];
+
+console.log(strList);
+
+
+// Tupple
+
+let ageName: [number, string];
+
+ageName = [21 ,'solve'];
+
+console.log(ageName);
+console.log(ageName[1].substr(1)); //->ovlve
+
+//error
+//let nameAge: [string, number] = [21 ,'solve'];
+
+// Enu,
+
+enum Color {Red = 12, Green, Blue}
+
+let c: Color = Color.Red;
+let colorName: string = Color[13];
+
+console.log(c);
+console.log(colorName);
+
+// Any
