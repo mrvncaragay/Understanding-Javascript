24ec7c5c59201d06f884c58a756ead9d389ff61e
Marvin Caragay <mrvncaragay@gmail.com>
2019-01-31 16:48:18 -0800

    tsc..

--- TypeScript/primitive-types.js
+++ TypeScript/primitive-types.js
@@ -1,37 +1,72 @@
-// Boolean
-var isDone = false;
-// Number
-var age = 30;
-var hex = 0xf00d;
-var binary = 10;
-var octal = 484;
-console.log(binary);
-console.log(octal);
-// String
-var myName = "Borlog!";
-console.log(myName);
-console.log("" + myName);
-// Array
-var ageList = [1, 3, 2];
-console.log(ageList);
-var strList = ["john", "borlog"];
-console.log(strList);
-// Tupple
-var ageName;
-ageName = [21, 'solve'];
-console.log(ageName);
-console.log(ageName[1].substr(1)); //->ovlve
-//error
-//let nameAge: [string, number] = [21 ,'solve'];
-// Enu,
-var Color;
-(function (Color) {
-    Color[Color["Red"] = 12] = "Red";
-    Color[Color["Green"] = 13] = "Green";
-    Color[Color["Blue"] = 14] = "Blue";
-})(Color || (Color = {}));
-var c = Color.Red;
-var colorName = Color[13];
-console.log(c);
-console.log(colorName);
-// Any
+// // Boolean
+//
+// let isDone: boolean = false;
+//
+// // Number
+//
+// let age: number = 30;
+// let hex: number = 0xf00d;
+// let binary: number = 0b1010;
+// let octal: number = 0o744;
+//
+// console.log(binary);
+// console.log(octal);
+//
+//
+// // String
+//
+// let myName: string = "Borlog!";
+//
+// console.log(myName);
+// console.log(`${myName}`);
+//
+// // Array
+// let ageList: number[] = [1, 3, 2];
+//
+// console.log(ageList);
+//
+// let strList: string[] = ["john", "borlog"];
+//
+// console.log(strList);
+//
+//
+// // Tupple
+//
+// let ageName: [number, string];
+//
+// ageName = [21 ,'solve'];
+//
+// console.log(ageName);
+// console.log(ageName[1].substr(1)); //->ovlve
+//
+// //error
+// //let nameAge: [string, number] = [21 ,'solve'];
+//
+// // Enum
+//
+// enum Color {Red = 1, Green, Blue}
+//
+// let c: Color = Color.Red;
+// let colorName: string = Color[1];
+//
+// console.log(c);
+// console.log(colorName);
+// Any - can be assign any primitive types
+// let notSure: any = 4;
+//
+// notSure = "Hoa";
+//
+// console.log(notSure);
+//
+// // Void - does not return a value
+//
+// function warUser(): void {
+//     console.log("Danger!");
+// }
+// Never
+// Type assertions <string> or as
+var someValue = "this is a string";
+var strLength = someValue.length;
+console.log(strLength);
+//let someValue: any = "this is a string";
+//let strLength: number = (someValue as string).length;
--- TypeScript/primitive-types.ts
+++ TypeScript/primitive-types.ts
@@ -1,55 +1,81 @@
-// Boolean
+// // Boolean
+//
+// let isDone: boolean = false;
+//
+// // Number
+//
+// let age: number = 30;
+// let hex: number = 0xf00d;
+// let binary: number = 0b1010;
+// let octal: number = 0o744;
+//
+// console.log(binary);
+// console.log(octal);
+//
+//
+// // String
+//
+// let myName: string = "Borlog!";
+//
+// console.log(myName);
+// console.log(`${myName}`);
+//
+// // Array
+// let ageList: number[] = [1, 3, 2];
+//
+// console.log(ageList);
+//
+// let strList: string[] = ["john", "borlog"];
+//
+// console.log(strList);
+//
+//
+// // Tupple
+//
+// let ageName: [number, string];
+//
+// ageName = [21 ,'solve'];
+//
+// console.log(ageName);
+// console.log(ageName[1].substr(1)); //->ovlve
+//
+// //error
+// //let nameAge: [string, number] = [21 ,'solve'];
+//
+// // Enum
+//
+// enum Color {Red = 1, Green, Blue}
+//
+// let c: Color = Color.Red;
+// let colorName: string = Color[1];
+//
+// console.log(c);
+// console.log(colorName);
 
-let isDone: boolean = false;
+// Any - can be assign any primitive types
+// let notSure: any = 4;
+//
+// notSure = "Hoa";
+//
+// console.log(notSure);
+//
+// // Void - does not return a value
+//
+// function warUser(): void {
+//     console.log("Danger!");
+// }
 
-// Number
+// Never
 
-let age: number = 30;
-let hex: number = 0xf00d;
-let binary: number = 0b1010;
-let octal: number = 0o744;
+// Type assertions <string> or as
 
-console.log(binary);
-console.log(octal);
+let someValue: any = "this is a string";
 
+let strLength: number = (someValue as string).length;
 
-// String
+console.log(strLength);
 
-let myName: string = "Borlog!";
-
-console.log(myName);
-console.log(`${myName}`);
-
-// Array
-let ageList: number[] = [1, 3, 2];
-
-console.log(ageList);
-
-let strList: string[] = ["john", "borlog"];
-
-console.log(strList);
+//let someValue: any = "this is a string";
+//let strLength: number = (someValue as string).length;
 
 
-// Tupple
-
-let ageName: [number, string];
-
-ageName = [21 ,'solve'];
-
-console.log(ageName);
-console.log(ageName[1].substr(1)); //->ovlve
-
-//error
-//let nameAge: [string, number] = [21 ,'solve'];
-
-// Enum
-
-enum Color {Red = 12, Green, Blue}
-
-let c: Color = Color.Red;
-let colorName: string = Color[13];
-
-console.log(c);
-console.log(colorName);
-
-// Any
