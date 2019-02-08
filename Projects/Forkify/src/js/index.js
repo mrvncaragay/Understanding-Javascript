f68250ce16bf65d0c537dcaac07e92f28cb95ebf
Marvin Caragay <mrvncaragay@gmail.com>
2019-02-07 16:28:54 -0800

    pagination buttons, loading spinner

--- Projects/Forkify/src/js/index.js
+++ Projects/Forkify/src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 
 
@@ -26,11 +26,13 @@ const controlSearch = async () => {
         // - Prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         // - Search for recipes
         await state.search.getResult();
 
         // - Render results on UI
+        clearLoader();
         searchView.renderResults(state.search.result);
     }
 };
@@ -40,4 +42,14 @@ elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
 
     controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    };
 });
