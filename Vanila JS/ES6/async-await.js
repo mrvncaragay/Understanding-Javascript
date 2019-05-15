2b85484f5aa91cde64bd13b7158562634d71f5c1
Marvin Caragay <mrvncaragay@gmail.com>
2019-05-15 10:57:01 -0700

    review promises and async-await

--- Vanila JS/ES6/async-await.js
+++ Vanila JS/ES6/async-await.js
@@ -1,57 +1,185 @@
-// Async/Await
-//
+// // Async/Await
+// //
 
 
-//-resolve -if the function is succesful
-//-reject - if it faileds
+// //-resolve -if the function is succesful
+// //-reject - if it faileds
 
-const getIDs = new Promise((resolve, reject) =>{
+// const getIDs = new Promise((resolve, reject) =>{
 
-    setTimeout(() =>{
-        resolve([12, 32, 123]); //succesfull
-        //reject([12, 32, 123]);  //failed
+//     setTimeout(() =>{
+//         resolve([12, 32, 123]); //succesfull
+//         //reject([12, 32, 123]);  //failed
 
-    }, 1500);
+//     }, 1500);
 
-});
+// });
 
-const getRecipe = recID => {
+// const getRecipe = recID => {
 
-    return new Promise((resolve, reject) =>{
+//     return new Promise((resolve, reject) =>{
 
-        setTimeout(ID => {
+//         setTimeout(ID => {
 
-            resolve('here is the recipe! ' + ID);
+//             resolve('here is the recipe! ' + ID);
 
-        }, 2000, recID);
+//         }, 2000, recID);
 
-    });
-};
+//     });
+// };
 
-const tellMeMore = str => {
+// const tellMeMore = str => {
 
-    return new Promise((resolve, reject) => {
+//     return new Promise((resolve, reject) => {
 
-        setTimeout(morestr => {
+//         setTimeout(morestr => {
 
-            resolve(morestr + " this is a new str from another promises");
+//             resolve(morestr + " this is a new str from another promises");
 
-        }, 2000, str)
+//         }, 2000, str)
 
-    });
+//     });
 
-};
+// };
 
-async function getRecipesAW() {
-    const IDs = await getIDs;
+// async function getRecipesAW() {
+//     const IDs = await getIDs;
 
-    console.log(IDs);
+//     console.log(IDs);
 
-    const recipe = await tellMeMore(IDs);
+//     const recipe = await tellMeMore(IDs);
 
-    return recipe;
+//     return recipe;
+// }
+
+// console.log("runner");
+// getRecipesAW().then(result => console.log(result));
+// console.log('breaker');
+
+
+
+// function getUser(id) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('Reading a user from a database...');
+//             resolve({ id: id, gitHubUsername: 'marv' });
+//         }, 2000);
+//     });
+// }
+
+// function getRepo(username) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('Calling Github API...');
+//             //resolve(['repo1', 'repo2', 'repo3']);
+//             reject(new Error('Lost Connection!'));
+//         }, 2000);
+//     });
+// }
+
+// //Promise-based approach
+// // getUser(1)
+// //     .then(user => getRepo(user.gitHubUsername))
+// //     .then(repos => console.log(repos))
+// //     .catch(err => console.log(err))
+
+// //Async-Awai approach
+// async function displayRepo() {
+//     try {
+//         const user = await getUser(1);
+//         const repo = await getRepo(user);
+//         console.log(repo);
+//     } catch (err) {
+//         console.log(err);
+//     }
+
+// }
+
+// displayRepo();
+
+//Async Await Exsercice
+
+// getCustomer(1, (customer) => {
+//     console.log('Customer: ', customer);
+//     if (customer.isGold) {
+//       getTopMovies((movies) => {
+//         console.log('Top movies: ', movies);
+//         sendEmail(customer.email, movies, () => {
+//           console.log('Email sent...')
+//         });
+//       });
+//     }
+//   });
+
+async function getNotify() {
+    try {
+        const cust = await getCustomer(1);
+        console.log('Customer: ', cust);
+        if(cust.isGold) {
+            const movies = await getTopMovies();
+            console.log('Top movies: ', movies);
+            await sendEmail(cust.email, movies);
+            console.log('Email sent...');
+    
+        }
+    } catch (err){
+        console.log(err);
+    }
 }
 
-console.log("runner");
-getRecipesAW().then(result => console.log(result));
-console.log('breaker');
+getNotify();
+
+
+  
+//   function getCustomer(id, callback) {
+//     setTimeout(() => {
+//       callback({ 
+//         id: 1, 
+//         name: 'Mosh Hamedani', 
+//         isGold: true, 
+//         email: 'email' 
+//       });
+//     }, 4000);  
+//   }
+
+  function getCustomer(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ 
+              id: 1, 
+              name: 'Mosh Hamedani', 
+              isGold: true, 
+              email: 'email' 
+            });
+          }, 4000);  
+    });
+  }
+  
+//   function getTopMovies(callback) {
+//     setTimeout(() => {
+//       callback(['movie1', 'movie2']);
+//     }, 4000);
+//   }
+  
+//   function sendEmail(email, movies, callback) {
+//     setTimeout(() => {
+//       callback();
+//     }, 4000);
+//   }
+
+  function getTopMovies() {
+    return new Promise((resolve, reject) => {  
+        setTimeout(() => {
+            resolve(['movie1', 'movie2']);
+        }, 4000);
+    });
+  }
+  
+  function sendEmail(email, movies) {
+      return new Promise((resolve, rejecr) => {
+        setTimeout(() => {
+            resolve();
+          }, 4000);
+      });
+  }
+
--- Vanila JS/ES6/promises.js
+++ Vanila JS/ES6/promises.js
@@ -1,75 +1,124 @@
-//Promises
-//
-// -Object that keeps track about whether a certain event has happened already or not.
-// -if yes, it determines what happens after the event has happened
-// -implements that concept of a future value that were expecting
+// //Promises
+// //
+// // -Object that keeps track about whether a certain event has happened already or not.
+// // -if yes, it determines what happens after the event has happened
+// // -implements that concept of a future value that were expecting
 
-//states
-// -Pending -before that event has happend
-// -Settled/Resolved -after the event has happened
-// -fulfilled -successful run
-// -Rejected -if error occurs
+// //states
+// // -Pending -before that event has happend
+// // -Settled/Resolved -after the event has happened
+// // -fulfilled -successful run
+// // -Rejected -if error occurs
 
-//has  two parameters
-//-resolve -if the function is succesful
-//-reject - if it faileds
+// //has  two parameters
+// //-resolve -if the function is succesful
+// //-reject - if it faileds
 
-const getIDs = new Promise((resolve, reject) =>{
+// //Promise.all([promise1, promise2,....]) -runs all promises and return an array of resolve values
+// //Promise.race([promise1, promise2,....]) -runs all promise and return an first resolve fullfilled promise
 
-    setTimeout(() =>{
-            resolve([12, 32, 123]); //succesfull
-            //reject([12, 32, 123]);  //failed
+// const getIDs = new Promise((resolve, reject) =>{
 
-    }, 1500);
+//     setTimeout(() =>{
+//             resolve([12, 32, 123]); //succesfull
+//             //reject([12, 32, 123]);  //failed
 
-});
+//     }, 1500);
 
-const getRecipe = recID => {
+// });
 
-    return new Promise((resolve, reject) =>{
+// const getRecipe = recID => {
 
-        setTimeout(ID => {
+//     return new Promise((resolve, reject) =>{
 
-            resolve('here is the recipe! ' + ID);
+//         setTimeout(ID => {
 
-        }, 2000, recID);
+//             resolve('here is the recipe! ' + ID);
 
-    });
-};
+//         }, 2000, recID);
 
-const tellMeMore = str => {
+//     });
+// };
 
+// const tellMeMore = str => {
+
+//     return new Promise((resolve, reject) => {
+
+//         setTimeout(morestr => {
+
+//             resolve(morestr + "this is a new str from another promises");
+
+//         }, 2000, str)
+
+//     });
+
+// };
+
+
+
+// //then -get the result
+// //.catch if promise failed
+// getIDs.then((IDs) =>{
+//     console.log(IDs);
+//     return getRecipe(IDs[1]);
+
+// }).then(recipe => {
+//     console.log(recipe);
+//     return tellMeMore(recipe);
+
+// }).then(mstr => {
+
+//     console.log(mstr);
+// })
+//     .catch(error => {
+//     console.log(error);
+// });
+
+// let flag = 0; // true
+
+// const p = new Promise((resolve, reject) => {
+//     if(flag) {
+//         setTimeout(() => {
+//             resolve('Connected to the database!');
+//         }, 2000);
+//     } else {
+//         setTimeout(() => {
+//             reject('Not connected to the database!');
+//         }, 2000);
+//     }
+// })
+
+// const l = p.then(result => {
+//     console.log(result);
+// }).catch(err => {
+//     console.log(err);
+// })
+
+
+///////////////////////////////////
+//  returning a promise to a function
+///////////////////////////////////
+function getUser(id) {
     return new Promise((resolve, reject) => {
-
-        setTimeout(morestr => {
-
-            resolve(morestr + "this is a new str from another promises");
-
-        }, 2000, str)
-
+        setTimeout(() => {
+            console.log('Reading a user from a database...');
+            resolve({ id: id, gitHubUsername: 'marv' });
+        }, 2000);
     });
+}
 
-};
-
-
-
-//then -get the result
-//.catch if promise failed
-getIDs.then((IDs) =>{
-    console.log(IDs);
-    return getRecipe(IDs[1]);
-
-}).then(recipe => {
-    console.log(recipe);
-    return tellMeMore(recipe);
-
-}).then(mstr => {
-
-    console.log(mstr);
-})
-    .catch(error => {
-    console.log(error);
-});
+function getRepo(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Calling Github API...');
+            resolve(['repo1', 'repo2', 'repo3']);
+            //reject(new Error('Lost Connection!'));
+        }, 2000);
+    });
+}
 
 
-
+getUser(1)
+    .then(user => getRepo(user.gitHubUsername))
+    .then(repos => console.log(repos))
+    .catch(err => console.log(err));
